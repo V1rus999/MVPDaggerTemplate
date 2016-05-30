360ab1dc162835177022d3f0835e9da13b5b7d0c
Johannes <h-annie@live.co.za>
2016-05-30 19:52:31 +0200

    2_Added dagger and some modules/components.

---
+++ MvcDaggerTemplate/app/src/main/java/com/droidit/mvcdaggertemplate/dependencyInjection/ApplicationComponent.java
@@ -0,0 +1,19 @@
+package com.droidit.mvcdaggertemplate.dependencyInjection;
+
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by V1rus on 30-May-16.
+ * Main component exposing graphs to rest of app
+ */
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+
+    Context context();
+
+}
