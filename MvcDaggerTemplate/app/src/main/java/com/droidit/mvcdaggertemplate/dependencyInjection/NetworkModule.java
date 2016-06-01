f2461a65ce8cbc3b85d2962867c6806f50600c69
Johannes <h-annie@live.co.za>
2016-06-01 17:41:37 +0200

    2_Added retrofit module.

---
+++ MvcDaggerTemplate/app/src/main/java/com/droidit/mvcdaggertemplate/dependencyInjection/NetworkModule.java
@@ -0,0 +1,15 @@
+package com.droidit.mvcdaggertemplate.dependencyInjection;
+
+import dagger.Module;
+
+/**
+ * Created by JohannesC on 01-Jun-16.
+ */
+@Module
+public class NetworkModule {
+
+//    http://jsonplaceholder.typicode.com/
+
+
+
+}
---
+++ MvcDaggerTemplate/retrofit/src/main/java/com/droidit/retrofit/PeopleService.java
@@ -0,0 +1,9 @@
+package com.droidit.retrofit;
+
+/**
+ * Created by JohannesC on 01-Jun-16.
+ */
+public interface PeopleService {
+    
+
+}
